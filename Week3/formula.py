402208ef3e21f5252f51d054cd346ac6ca8b6cf0
Harrinive <dychen1999@foxmail.com>
2020-03-21 12:51:25 +0800

    week3

---
+++ Week3/formula.py
@@ -0,0 +1,25 @@
+import numpy as np 
+def getSolution1(parameter):
+    a, b, c = parameter
+    delta = np.sqrt(b**2 - 4*a*c)
+    return (-b + delta)/(2*a), (-b - delta)/(2*a)
+
+def getSolution2(parameter):
+    a, b, c = parameter
+    delta = np.sqrt(b**2 - 4*a*c)
+    return 2*c/(-b + delta), 2*c/(-b - delta)
+
+def getPresiceSolution(parameter):
+    a, b, c = parameter
+    delta = np.sqrt(b**2 - 4*a*c)
+    if b >= 0:
+        dom = -b - delta
+        return dom / 2 / a, 2 * c / dom
+    else:
+        dom = -b + delta
+        return dom / 2 / a, 2 * c / dom
+
+if __name__ == '__main__':
+    print(getSolution1([1, -10000.0001, 1]))
+    print(getSolution2([1, -10000.0001, 1]))
+    print(getPresiceSolution([1, -10000.0001, 1]))
